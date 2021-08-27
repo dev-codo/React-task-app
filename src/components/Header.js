efc64715f14e258a94739c63c167fea2d7079cf1
dev-codo <dev.codo@gmail.com>
2021-08-26 23:25:06 -0400

    Remove dynamic styling inside Js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
     return (
         <header>
-            <h1 style={headingStyle}>{title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
@@ -18,10 +18,10 @@ Header.propTypes = {
     title: PropTypes.string
 }
 
-// CSS in JS: mostly for dynamic styling
-const headingStyle = {
-    color: 'red',
-    backgroundColor: 'yellow'
-}
+// // CSS in JS: mostly for dynamic styling
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'yellow'
+// }
 
 export default Header
