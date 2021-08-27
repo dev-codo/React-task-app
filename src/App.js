f6231b41ac5f83ad9a66980efe306e8d2498f8be
dev-codo <dev.codo@gmail.com>
2021-08-27 12:09:02 -0400

    AddTask component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
@@ -24,6 +25,12 @@ function App() {
         }
     ]);
 
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...tasks, newTask])
+    }
+
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id));
     }
@@ -39,6 +46,7 @@ function App() {
     return (
         <div className="container">
             <Header />
+            <AddTask onAdd={addTask} />
             {tasks.length > 0 ?
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggleReminder={toggleReminder} />
                 : 'Nao ha task para mostrar'
