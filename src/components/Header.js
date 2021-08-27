08a6f248d2e55de250877bc598cefd9bfee3ad2e
dev-codo <dev.codo@gmail.com>
2021-08-26 23:24:24 -0400

    Initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
+    return (
+        <header>
+            <h1 style={headingStyle}>{title}</h1>
+        </header>
+    )
+}
+
+// If no attribute title is passed on the component
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string
+}
+
+// CSS in JS: mostly for dynamic styling
+const headingStyle = {
+    color: 'red',
+    backgroundColor: 'yellow'
+}
+
+export default Header
